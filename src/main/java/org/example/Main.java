7de07c0d7bec751240914557ea6580d199c3c174
Batuhan Karakaş <karakasbatuhan96@gmail.com>
2024-10-26 09:30:52 +0300

    Java-Project-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,58 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if(isBarking == true && clock > 20){
+            isBarking = true;
+        }
+        else if(isBarking && clock < 8){
+            isBarking = true;
+        }
+        else{
+            isBarking = false;
+        }
+        return isBarking;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       if(firstAge >= 13 && firstAge <= 19){
+            return true;
+       }
+       if(secondAge >= 13 && secondAge <= 19){
+            return true;
+       }
+       if(thirdAge >= 13 && thirdAge <= 19){
+            return true;
+       }
+       else {
+           return false;
+       }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if(isSummer){
+            if(temp >= 35 && temp <= 45){
+                return true;
+            }
+        }
+        else if(!isSummer){
+            if(temp >= 25 && temp <= 35){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static double area(double width, double height) {
-       
+       if(width >= 0 && height >= 0){
+           return width * height;
+       }
+       return -1;
     }
 
     public static double area(double radius) {
-       
+       if(radius >= 0){
+           return radius*radius*Math.PI;
+       }
+       return -1;
     }
 }
